bc914eedafd3dbbc67b799db7bb8f9c56766db1e
Sezgin <Sezgin@DESKTOP-A0SS61H>
2025-01-09 11:09:53 +0300

    Add project files.

---
+++ ToDoListApplicationV1/Form1.Designer.cs
@@ -0,0 +1,305 @@
+﻿namespace ToDoListApplicationV1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.rchDescription = new System.Windows.Forms.RichTextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnToday = new System.Windows.Forms.Button();
+            this.btnTomorrow = new System.Windows.Forms.Button();
+            this.btnAll = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.monthCalendar2 = new System.Windows.Forms.MonthCalendar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.lblid = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.BackColor = System.Drawing.SystemColors.Info;
+            this.monthCalendar1.Location = new System.Drawing.Point(43, 9);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 0;
+            this.monthCalendar1.DateChanged += new System.Windows.Forms.DateRangeEventHandler(this.monthCalendar1_DateChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(319, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 24);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Add New";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(319, 50);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(57, 24);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Title :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(319, 98);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(114, 24);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Description :";
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Location = new System.Drawing.Point(441, 50);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(283, 32);
+            this.txtTitle.TabIndex = 8;
+            // 
+            // rchDescription
+            // 
+            this.rchDescription.Location = new System.Drawing.Point(441, 102);
+            this.rchDescription.Name = "rchDescription";
+            this.rchDescription.Size = new System.Drawing.Size(838, 125);
+            this.rchDescription.TabIndex = 9;
+            this.rchDescription.Text = "";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(1133, 233);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(146, 47);
+            this.btnAdd.TabIndex = 10;
+            this.btnAdd.Text = "ADD";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnToday
+            // 
+            this.btnToday.BackColor = System.Drawing.Color.Turquoise;
+            this.btnToday.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnToday.Location = new System.Drawing.Point(43, 286);
+            this.btnToday.Name = "btnToday";
+            this.btnToday.Size = new System.Drawing.Size(262, 47);
+            this.btnToday.TabIndex = 11;
+            this.btnToday.Text = "TODAY";
+            this.btnToday.UseVisualStyleBackColor = false;
+            this.btnToday.Click += new System.EventHandler(this.btnToday_Click);
+            // 
+            // btnTomorrow
+            // 
+            this.btnTomorrow.BackColor = System.Drawing.Color.Turquoise;
+            this.btnTomorrow.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnTomorrow.Location = new System.Drawing.Point(43, 339);
+            this.btnTomorrow.Name = "btnTomorrow";
+            this.btnTomorrow.Size = new System.Drawing.Size(262, 47);
+            this.btnTomorrow.TabIndex = 12;
+            this.btnTomorrow.Text = "This Week";
+            this.btnTomorrow.UseVisualStyleBackColor = false;
+            this.btnTomorrow.Click += new System.EventHandler(this.btnTomorrow_Click);
+            // 
+            // btnAll
+            // 
+            this.btnAll.BackColor = System.Drawing.Color.BlueViolet;
+            this.btnAll.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAll.ForeColor = System.Drawing.Color.White;
+            this.btnAll.Location = new System.Drawing.Point(43, 392);
+            this.btnAll.Name = "btnAll";
+            this.btnAll.Size = new System.Drawing.Size(262, 47);
+            this.btnAll.TabIndex = 13;
+            this.btnAll.Text = "ALL";
+            this.btnAll.UseVisualStyleBackColor = false;
+            this.btnAll.Click += new System.EventHandler(this.btnAll_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.dataGridView1);
+            this.panel1.Location = new System.Drawing.Point(323, 286);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(966, 429);
+            this.panel1.TabIndex = 14;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(966, 429);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(991, 721);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(146, 47);
+            this.btnEdit.TabIndex = 15;
+            this.btnEdit.Text = "EDIT";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(839, 721);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(146, 47);
+            this.btnDelete.TabIndex = 16;
+            this.btnDelete.Text = "DELETE";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(1143, 721);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(146, 47);
+            this.button7.TabIndex = 17;
+            this.button7.Text = "COMPLETED";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // monthCalendar2
+            // 
+            this.monthCalendar2.BackColor = System.Drawing.SystemColors.Info;
+            this.monthCalendar2.Location = new System.Drawing.Point(43, 508);
+            this.monthCalendar2.Name = "monthCalendar2";
+            this.monthCalendar2.TabIndex = 18;
+            this.monthCalendar2.DateChanged += new System.Windows.Forms.DateRangeEventHandler(this.monthCalendar2_DateChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 475);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(210, 24);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "Choose From Calendar :";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(981, 233);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(146, 47);
+            this.btnSave.TabIndex = 20;
+            this.btnSave.Text = "SAVE";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Visible = false;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // lblid
+            // 
+            this.lblid.AutoSize = true;
+            this.lblid.Location = new System.Drawing.Point(289, 256);
+            this.lblid.Name = "lblid";
+            this.lblid.Size = new System.Drawing.Size(16, 24);
+            this.lblid.TabIndex = 21;
+            this.lblid.Text = "-";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(125)))), ((int)(((byte)(142)))), ((int)(((byte)(171)))));
+            this.ClientSize = new System.Drawing.Size(1334, 888);
+            this.Controls.Add(this.lblid);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.monthCalendar2);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnAll);
+            this.Controls.Add(this.btnTomorrow);
+            this.Controls.Add(this.btnToday);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.rchDescription);
+            this.Controls.Add(this.txtTitle);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.monthCalendar1);
+            this.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "To Do List";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.RichTextBox rchDescription;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnToday;
+        private System.Windows.Forms.Button btnTomorrow;
+        private System.Windows.Forms.Button btnAll;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.MonthCalendar monthCalendar2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label lblid;
+    }
+}
+
---
+++ ToDoListApplicationV1/Form1.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace ToDoListApplicationV1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection connection = new SqlConnection("Data Source=DESKTOP-A0SS61H\\SQLEXPRESS01;Initial Catalog=todo_list;Integrated Security=True;");
+
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ShowData();
+        }
+        void ShowData()
+        {
+            try
+            {
+                string q = "SELECT *FROM tbl_Todolist ORDER BY DAY ASC";
+                SqlDataAdapter da = new SqlDataAdapter(q, connection);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                if(dt.Rows.Count > 0 )
+                {
+                    dataGridView1.DataSource = dt;
+                    dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+                    dataGridView1.ClearSelection();
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void AddTodoList()
+        {
+            
+            DateTime selectedDate = monthCalendar1.SelectionStart.Date;
+            connection.Open();
+            string query = "INSERT INTO tbl_Todolist (Description, Title, DAY, Status) VALUES (@desc, @title, @day, @stat)";
+            SqlCommand command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@desc", rchDescription.Text);
+            command.Parameters.AddWithValue("@title", txtTitle.Text);
+            command.Parameters.AddWithValue("@stat", "Continues");
+            command.Parameters.AddWithValue("@day", selectedDate);
+            command.ExecuteNonQuery();
+            connection.Close();
+            ShowData();
+            txtTitle.Text = "";
+            rchDescription.Text = "";
+
+            
+        }
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+                AddTodoList();
+        }
+
+        private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
+        {
+            
+            
+        }
+
+        private void btnToday_Click(object sender, EventArgs e)
+        {
+            DateTime todayy = DateTime.Today;
+            connection.Open();
+            SqlCommand command = new SqlCommand("SELECT * FROM tbl_Todolist WHERE CONVERT(date, DAY) = @day", connection);
+            command.Parameters.AddWithValue("@day", todayy);
+
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+
+            dataGridView1.DataSource = dt;
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView1.ClearSelection();
+
+            connection.Close();
+        }
+
+        private void monthCalendar2_DateChanged(object sender, DateRangeEventArgs e)
+        {
+           DateTime selectedDayforChoose = monthCalendar2.SelectionStart.Date;
+            connection.Open();
+            SqlCommand command = new SqlCommand("SELECT * FROM tbl_Todolist WHERE CONVERT(date, DAY) = @day", connection);
+            command.Parameters.AddWithValue("@day", selectedDayforChoose);
+
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+
+            dataGridView1.DataSource = dt;
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView1.ClearSelection();
+
+            connection.Close();
+        }
+
+        private void btnTomorrow_Click(object sender, EventArgs e)
+        {
+            connection.Open();
+            SqlCommand command = new SqlCommand("SELECT * FROM tbl_Todolist WHERE DATEPART(WEEK, DAY) = DATEPART(WEEK, GETDATE())", connection);
+
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+
+            dataGridView1.DataSource = dt;
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView1.ClearSelection();
+
+            connection.Close();
+        }
+
+        private void btnAll_Click(object sender, EventArgs e)
+        {
+            ShowData();
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            int selectedCell = dataGridView1.SelectedCells[0].RowIndex;
+            txtTitle.Text = dataGridView1.Rows[selectedCell].Cells[2].Value.ToString();
+            rchDescription.Text = dataGridView1.Rows[selectedCell].Cells[1].Value.ToString();
+            btnSave.Visible = true;
+            btnAdd.Enabled = false;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            DateTime selectedDate = monthCalendar1.SelectionStart.Date;
+            string query = "UPDATE tbl_Todolist SET Description=@desc, Title=@title, DAY=@day WHERE ID=@p1";
+            connection.Open();
+            SqlCommand command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@desc",rchDescription.Text);
+            command.Parameters.AddWithValue("@title", txtTitle.Text);
+            command.Parameters.AddWithValue("@day", selectedDate);
+            command.Parameters.AddWithValue("@p1",lblid.Text);
+            command.ExecuteNonQuery();
+            ShowData();
+            btnAdd.Enabled = true;
+            btnSave.Visible = false;
+            connection.Close();
+
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dataGridView1.SelectedCells.Count > 0)
+            {
+                int selectedCell1 = dataGridView1.SelectedCells[0].RowIndex;
+                lblid.Text = dataGridView1.Rows[selectedCell1].Cells[0].Value.ToString();
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+           if(lblid.Text == "-" || lblid.Text == "")
+            {
+                MessageBox.Show("Please select an item to be deleted.");
+            }
+           else
+            {
+                DialogResult result = MessageBox.Show("The selected item will be deleted. Are you sure?","WARNING",MessageBoxButtons.YesNo,MessageBoxIcon.Warning);
+                if(result == DialogResult.Yes)
+                {
+                    try
+                    {
+                        string query = "DELETE FROM tbl_Todolist WHERE ID=@p1";
+                        connection.Open();
+                        SqlCommand delete = new SqlCommand(query, connection);
+                        delete.Parameters.AddWithValue("@p1", lblid.Text);
+                        delete.ExecuteNonQuery();
+                        
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Something went wrong." + ex.Message);
+                    }
+                    finally
+                    {
+                        connection.Close();
+                        ShowData();
+
+                    }
+                }
+                
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (lblid.Text == "-" || lblid.Text == "")
+            {
+                MessageBox.Show("Please select an item");
+            }
+            else
+            {
+                try
+                {
+                    connection.Open();
+                    SqlCommand change = new SqlCommand("UPDATE tbl_Todolist SET Status=@status WHERE ID=@id",connection);
+                    change.Parameters.AddWithValue("@status", "COMPLETED");
+                    change.Parameters.AddWithValue("@id", lblid.Text);
+                    change.ExecuteNonQuery();
+                    ShowData();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Something went wrong." + ex.Message);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+            }
+        }
+    }
+}
